c76c886c7f1bee182a8c125fe3d4991b188d9b4f
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2023-09-25 15:48:49 -0300

    feat: Estruturando gráficos

--- appGanhosPorMes/src/Tabs/Home/index.tsx
+++ appGanhosPorMes/src/Tabs/Home/index.tsx
@@ -1,11 +1,61 @@
-import React from "react";
-import {Text} from "native-base"
+import React, { useState, useEffect } from "react";
+import { VStack} from "native-base"
+import { servicos } from "../../utils/api";
+import { BarChart } from "react-native-chart-kit";
+import { Dimensions } from "react-native";
+
 
 export const Home = () => {
+  const [byService, setByService] = useState([])
 
-    return(
-        <Text>
-            Página home page
-        </Text>
-    )
+  useEffect( () => {
+     function getService() {
+       const filterServicos = servicos.filter( (item) => item.valueTotalByService !== undefined)
+       setByService(filterServicos)
+    }
+    getService()
+  }, [])
+
+
+    return (
+      <VStack flex={1}>
+        <BarChart
+          data={{
+            labels: ["2023/08", "2023/09", "2023/09", "2023/10"],
+            datasets: [
+              {
+                data: byService.map((item) => item.valueTotalByService),
+              },
+            ],
+          }}
+          width={Dimensions.get("window").width} 
+          height={220}
+          yAxisLabel="$"
+          yAxisSuffix="R"
+          yAxisInterval={1} 
+          chartConfig={{
+            backgroundColor: "#fff",
+            backgroundGradientFrom: "#fff",
+            backgroundGradientTo: "#ffff",
+            decimalPlaces: 2, 
+            color: () => `#198EB6`,
+            labelColor: () => `#0f4a5e`,
+            style: {
+              borderRadius: 16,
+            },
+            propsForDots: {
+              r: "6",
+              strokeWidth: "2",
+              stroke: "#ffa726",
+            },
+          }}
+       
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
+
+      </VStack>
+    );
 }
---
+++ appGanhosPorMes/src/utils/api.ts
@@ -0,0 +1,37 @@
+export const servicos = [
+  {
+    id: "1f2d951f-d8a9-4648-85b2-5ee8caf0944b",
+    typeService: "corte simples",
+    valueService: 20,
+    amount: 3,
+    dataTracker: "2023-08-17T13:05:43.000Z",
+    valueTotalByService: 60,
+  },
+  {
+    id: "45cb5bf9-a53d-49db-9597-410227b3ecd4",
+    typeService: "corte simples",
+    valueService: 20,
+    amount: 5,
+    dataTracker: "2023-08-16T22:25:40.000Z",
+    valueTotalByService: 100,
+  },
+  {
+    id: "4cfb63e9-065d-47e2-9d65-8e4b3d6ac827",
+    typeService: "corte completo",
+    valueService: 30,
+    amount: 2,
+    dataTracker: "2023-08-21T16:26:34.000Z",
+    valueTotalByService: 60,
+  },
+  {
+    id: "980e9afa-a62b-45cd-a36d-b9965629f2e5",
+    typeService: "corte completo",
+    valueService: 30,
+    amount: 6,
+    dataTracker: "2023-08-16T22:25:18.000Z",
+    valueTotalByService: 180,
+  },
+  {
+    valorTotal: 400,
+  },
+];
